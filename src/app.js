43b81157a7c4b8dd08f48926fed891d60748d0f9
Manish Neupane <2naligaming2002@gmail.com>
2026-01-21 01:40:33 -0500

    chore(db): change server port to 5000

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Internal Server Error" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
